36ff22116cefaa6abab276978595ed5a00af2982
= <achla95@protonmail.com>
2023-03-04 17:07:01 +0100

    added shop menu

--- menu/mainMenu.js
+++ menu/mainMenu.js
@@ -5,6 +5,7 @@ import { userStats } from "../utils/userAndMobsStats.js"
 import { battleMenu } from "./battleMenu.js"
 import fight from "../utils/fight.js"
 import exitMenu from "./exitMenu.js"
+import shopMenu from "./shopMenu.js"
 
 const mainMenuAndUserChoice = async () => {
   textBoxMenu("Main menu")
@@ -20,12 +21,15 @@ const mainMenuAndUserChoice = async () => {
   console.log("3. Save game 💾")
   console.log("4. Exit ❌")
   const choice = Number(await ask("Your choice (1-4) : "))
-  if (choice < 1 || choice > 4 || isNaN(choice)) {
+  const mini = 1
+  const maxi = 4
+  if (choice < mini || choice > maxi || isNaN(choice)) {
     return mainMenuAndUserChoice()
   } else if (choice === 1) {
     battleMenu()
     fight()
   } else if (choice === 2) {
+    shopMenu()
   } else if (choice === 3) {
   } else {
     exitMenu()
@@ -34,6 +38,5 @@ const mainMenuAndUserChoice = async () => {
 
 export default mainMenuAndUserChoice
 
-/*petit bug jsp d'ou il vient qui fait que si je rentre 6 ou autre ca me met thank for playing 
-alors que non enft
-*/
+/*
+ */
